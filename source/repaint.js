a2028b2e536bd152df5d5dd4292a6720c1f55bed
N Bhargav <bhargav3@gmail.com>
2013-05-31 20:22:24 +0530

    Three rectangles with colors RGB are rendered in the order of object creation.

--- source/repaint.js
+++ source/repaint.js
@@ -2,15 +2,32 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-var c=document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
+function Rectangle(x, y, w, h, fill) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.fill = fill;
+}
 
-// Create gradient
-/* var grd=ctx.createLinearGradient(0,0,200,0);
-grd.addColorStop(0,"red");
-grd.addColorStop(1,"white"); */
+// get canvas element.
+var elem = document.getElementById('myCanvas');
 
-// Fill with gradient
-ctx.fillStyle=grd;
-ctx.fillRect(10,10,150,80);
+// check if context exist
+if (elem.getContext) {
 
+    var myRects = [];
+
+    myRects.push(new Rectangle(10, 10, 10, 20, "red"));
+    myRects.push(new Rectangle(0, 20, 20, 30, "blue"));
+    myRects.push(new Rectangle(40, 40, 30, 40, "green"));
+
+    context = elem.getContext('2d');
+    for (var i in myRects) {
+        objRect = myRects[i];
+        context.fillStyle = objRect.fill;
+        context.fillRect(objRect.x, objRect.y, objRect.w, objRect.h);
+
+    }
+
+}

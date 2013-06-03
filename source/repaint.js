24b83ba494b0ca6a1a7cdfb7ae66e9f5f79d2001
N Bhargav <bhargav3@gmail.com>
2013-06-03 18:31:25 +0530

    Cleared up bits of code and modularized it. Now interfaces moveTo(x,y) function.

--- source/repaint.js
+++ source/repaint.js
@@ -2,6 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+var myRects = [];
+
 function Rectangle(x, y, w, h, fill) {
     this.x = x;
     this.y = y;
@@ -11,36 +14,75 @@ function Rectangle(x, y, w, h, fill) {
 }
 
 Rectangle.associateWithCanvas = function(myCanvasElement) {
+    myRects = [];
     canvas = document.getElementById(myCanvasElement);
+    context = canvas.getContext('2d');
+    Rectangle.clear();
+    myRects.push(new Rectangle(10, 10, 20, 20, 'red'));
+    myRects.push(new Rectangle(20, 20, 30, 30, 'blue'));
+    myRects.push(new Rectangle(5, 30, 40, 40, 'green'));
+    myRects.push(new Rectangle(30, 10, 20, 20, 'violet'));
+    myRects.push(new Rectangle(40, 20, 30, 30, 'black'));
+    myRects.push(new Rectangle(50, 30, 40, 40, 'orange'));
+    for(var i in myRects){
+        myRects[i].draw();
+    }
+};
+
+Rectangle.clear = function(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
 };
 
 Rectangle.prototype.moveTo = function(x,y){
+    var counter = [];
+    for(var i in myRects){
+        if(!(this.x + this.w < myRects[i].x ||
+            myRects[i].x + myRects[i].width < this.x ||
+            this.y + this.h < myRects[i].y ||
+            myRects[i].y + myRects[i].h < this.y)){
+            
+                counter.push(i);
+                myRects[i].clearRect();
+            
+        }
+    }
+    //this.clearRect();
+    
     this.x = x;
     this.y = y;
-    clear();
-    draw();
-};
-
-var myRects = [];
-
-myRects.push(new Rectangle(10, 10, 20, 20, '#ff0000'));
-myRects.push(new Rectangle(20, 20, 30, 30, '#00ff00'));
-myRects.push(new Rectangle(70, 70, 40, 40, '#0000ff'));
     
+    for(var i in myRects){
+        if(!(this.x + this.w < myRects[i].x ||
+            myRects[i].x + myRects[i].width < this.x ||
+            this.y + this.h < myRects[i].y ||
+            myRects[i].y + myRects[i].h < this.y)){
+            
+                counter.push(i);
+                myRects[i].clearRect();
+            
+        }
+    }
+    
+    for(var i in counter){
+        console.log(counter[i]);
+        myRects[counter[i]].draw();
+    }
+    counter = [];
+    
+    //this.draw();
+};
 
-
-//myRects[0].moveTo(90,90);
-
-function draw(){
+Rectangle.prototype.draw = function(){
     context = canvas.getContext('2d');
-    for (var i in myRects) {
-        objRect = myRects[i];
-        context.fillStyle = objRect.fill;
-        context.fillRect(objRect.x, objRect.y, objRect.w, objRect.h);
-}
+    context.fillStyle = this.fill;
+    context.fillRect(this.x, this.y, this.w, this.h);
+};
+
+Rectangle.prototype.clearRect = function(){
+    context = canvas.getContext('2d');
+    context.fillStyle = 'white';
+    context.fillRect(this.x, this.y, this.w, this.h);
 };
 
 
-function clear(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-}
+

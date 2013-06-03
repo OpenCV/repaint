00a8ed42143f9f9a616253ef042952746d48b708
N Bhargav <bhargav3@gmail.com>
2013-06-03 11:23:05 +0530

    Added the functions draw(), clear() and moveTo() which will let's us add rectangles to the canvas and move them.

--- source/repaint.js
+++ source/repaint.js
@@ -10,24 +10,33 @@ function Rectangle(x, y, w, h, fill) {
     this.fill = fill;
 }
 
-// get canvas element.
-var elem = document.getElementById('myCanvas');
+Rectangle.associateWithCanvas = function(myCanvasElement) {
+    canvas = document.getElementById(myCanvasElement);
+};
 
-// check if context exist
-if (elem.getContext) {
+var myRects = [];
 
-    var myRects = [];
+myRects.push(new Rectangle(10, 10, 20, 20, '#ff0000'));
+myRects.push(new Rectangle(20, 20, 30, 30, '#00ff00'));
+myRects.push(new Rectangle(70, 70, 40, 40, '#0000ff'));
+    
+function moveTo(i, x, y){
+    myRects[i].x = x;
+    myRects[i].y = y;
+    clear();
+    draw();
+}
 
-    myRects.push(new Rectangle(10, 10, 10, 20, "red"));
-    myRects.push(new Rectangle(0, 20, 20, 30, "blue"));
-    myRects.push(new Rectangle(40, 40, 30, 40, "green"));
-
-    context = elem.getContext('2d');
+function draw(){
+    context = canvas.getContext('2d');
     for (var i in myRects) {
         objRect = myRects[i];
         context.fillStyle = objRect.fill;
         context.fillRect(objRect.x, objRect.y, objRect.w, objRect.h);
+}
+};
 
-    }
 
+function clear(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
